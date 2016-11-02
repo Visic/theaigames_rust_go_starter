994caecd0dbf651ed210ad2a7c4ebf2d26cb2309
Andrew <andrew.frailing@promessinc.com>
2016-11-02 15:57:57 -0400

    Initial

---
+++ src/game_state.rs
@@ -0,0 +1,37 @@
+use num::FromPrimitive;
+
+enum_from_primitive! {
+    pub enum Point {
+        Illegal = -1,
+        Empty,
+        Player1,
+        Player2,
+    }
+}
+
+pub struct GameState {
+    pub round: i32,
+    pub moves_done: i32,
+    pub player1_points: f64,
+    pub player2_points: f64,
+    pub field: Vec<Point>
+}
+
+impl GameState {
+    pub fn new() -> GameState {
+        GameState{ round: 0, moves_done: 0, player1_points: 0.0, player2_points: 0.0, field: vec![] }
+    }
+    
+    pub fn update(&mut self, category: &str, setting: &str, value_str: &str) {
+        match setting.as_ref() {
+            "round"                              => self.round = value_str.parse::<i32>().unwrap(),
+            "move"                               => self.moves_done = value_str.parse::<i32>().unwrap(),
+            "points"    if category == "player1" => self.player1_points = value_str.parse::<f64>().unwrap(),
+            "points"    if category == "player2" => self.player2_points = value_str.parse::<f64>().unwrap(),
+            "last_move" if category == "player1" => { /*example value_str: place_move 15 1*/ },
+            "last_move" if category == "player2" => { /*example value_str: place_move 15 1*/ },
+            "field"                              => self.field = value_str.split(",").map(|x| Point::from_i32(x.parse::<i32>().unwrap()).unwrap()).collect(),
+            _ => {}
+        }
+    }
+}
---
+++ src/settings.rs
@@ -0,0 +1,30 @@
+pub struct Settings {
+    pub timebank: i32,
+    pub time_per_move: i32,
+    pub player_names: Vec<String>,
+    pub your_bot: String,
+    pub your_botid: i32,
+    pub field_width: i32,
+    pub field_height: i32,
+    pub max_rounds: i32
+}
+
+impl Settings {
+    pub fn new() -> Settings {
+        Settings{ timebank: 0, time_per_move: 0, player_names: vec![], your_bot: String::from(""), your_botid: 0, field_width: 0, field_height: 0, max_rounds: 0 }
+    }
+    
+    pub fn update(&mut self, setting: &str, value_str: &str) {
+        match setting.as_ref() {
+            "timebank"      => self.timebank = value_str.parse::<i32>().unwrap(),
+            "time_per_move" => self.time_per_move = value_str.parse::<i32>().unwrap(),
+            "player_names"  => self.player_names = value_str.split(",").map(|x| String::from(x)).collect(),
+            "your_bot"      => self.your_bot = String::from(value_str),
+            "your_botid"    => self.your_botid = value_str.parse::<i32>().unwrap(),
+            "field_width"   => self.field_width = value_str.parse::<i32>().unwrap(),
+            "field_height"  => self.field_height = value_str.parse::<i32>().unwrap(),
+            "max_rounds"    => self.max_rounds = value_str.parse::<i32>().unwrap(),
+            _ => {}
+        }
+    }
+}
